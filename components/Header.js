32b59a9db90f02d2148b364d36bf8fab4b10ac66
Okky Maheswara <okkymahes@gmail.com>
2022-07-21 20:24:41 +0800

    lagi ulang deploy 2

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,26 @@
-import Image from 'next/image'
-import CTA from './CTA'
-import HeaderSocial from './HeaderSocial'
+import Image from "next/image"
+import CTA from "./CTA"
+import HeaderSocial from "./HeaderSocial"
 import ME from "../public/assets/me.png";
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='h-screen pt-28 overflow-hidden'>
-      <div className='text-center h-full relative'>
+    <header className="h-screen pt-28 overflow-hidden">
+      <div className="text-center h-full relative">
         <h5>Hello I'm</h5>
         <h1>Okky Maheswara</h1>
-        <h5 className='text-light'>Full Stack Developer</h5>
+        <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <HeaderSocial />
 
-        <div className='bg-gradient-to-b from-primary to-primary/20 w-80 h-96 absolute left-1/2 transform -translate-x-1/2 mt-16 rounded-t-[12rem] overflow-hidden pt-20 px-6 pb-6'>
-          <Image alt='My Profile' src={ME} />
+        <div className="bg-gradient-to-b from-primary to-primary/20 w-80 h-96 absolute left-1/2 transform -translate-x-1/2 mt-16 rounded-t-[12rem] overflow-hidden pt-20 px-6 pb-6">
+          {/* <img src={ME} alt="My Profile" /> */}
+          <Image alt="My Profile" src={ME} />
         </div>
 
-        <Link href='#contact'>
-          <a className='absolute -right-9 bottom-20 rotate-90 font-light hidden sm:flex'>Scroll Down</a>
+        <Link href="#contact">
+          <a className="absolute -right-9 bottom-20 rotate-90 font-light hidden sm:flex">Scroll Down</a>
         </Link>
       </div>
     </header>
