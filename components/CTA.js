fd72b0612fd13a82f4777381da7dabca90f9c1e4
Okky Maheswara <okkymahes@gmail.com>
2022-07-21 20:00:33 +0800

    lagi ulang deploy 1

--- components/CTA.js
+++ components/CTA.js
@@ -1,16 +1,14 @@
 import Link from "next/link"
 
-const CTA = () => {
+export default function CTA() {
   return (
-    <div className='mt-10 flex gap-5 justify-center'>
-      <Link href='/assets/cv.pdf'>
-        <a className='btn'>Download CV</a>
+    <div className="mt-10 flex gap-5 justify-center">
+      <Link href="/assets/cv.pdf">
+        <a className="btn">Download CV</a>
       </Link>
-      <Link href='#contact'>
-        <a className='btn btn-primary'>Let's Talk</a>
+      <Link href="#contact">
+        <a className="btn btn-primary">Let's Talk</a>
       </Link>
     </div>
   )
 }
-
-export default CTA
