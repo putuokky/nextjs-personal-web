119246d04a6834f687ba43d9bda6504a4c35b16b
Okky Maheswara <okkymahes@gmail.com>
2022-07-23 13:03:48 +0800

    section about done

--- components/About.js
+++ components/About.js
@@ -8,26 +8,26 @@ export default function About() {
     <section id='about'>
       <h5>Get to Know</h5>
       <h2>About Me</h2>
-      <div className='about_container grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-[15%]'>
-        <div className='about_me w-2/3 mt-8 mx-auto mb-12 aspect-square rounded-[2rem] bg-gradient-to-b from-primary to-primary/20 grid place-items-center md:mx-auto md:mt-4 md:mb-6 md:w-1/2 lg:w-full lg:mt-2 lg:mb-4'>
-          <div className='about_me_image rounded-[2rem] overflow-hidden rotate-12 duration-300 ease-out hover:rotate-0'>
+      <div className='grid grid-cols-1 gap-0 lg:grid-cols-2'>
+        <div className='w-2/3 mt-8 mx-auto mb-16 aspect-square rounded-[2rem] bg-gradient-to-b from-primary to-primary/20 grid place-items-center lg:mt-0'>
+          <div className='rounded-[2rem] overflow-hidden rotate-12 duration-300 ease-out hover:rotate-0'>
             <img src="/assets/me-about.jpg" alt='About Image' />
           </div>
         </div>
         <div className='about_content'>
-          <div className='about_cards grid grid-cols-1 gap-6 md:grid-cols-3'>
-            <article className='about_card bg-tema-secondary border-[1px] border-solid border-transparent rounded-2xl p-8 text-center hover:bg-transparent hover:border-tema-secondary hover:cursor-default'>
-              <BsAward className='about_icon text-primary text-2xl mb-4 mx-auto' />
+          <div className='grid grid-cols-1 gap-6 w-3/4 mx-auto sm:grid-cols-2 sm:w-full md:grid-cols-3'>
+            <article className='bg-tema-secondary border-[1px] border-solid border-transparent rounded-2xl p-8 text-center hover:bg-transparent hover:border-tema-secondary hover:cursor-default'>
+              <BsAward className='text-primary text-2xl mb-4 mx-auto' />
               <h5 className="text-base">Experience</h5>
               <small className="text-xs text-light">3+ years Working</small>
             </article>
-            <article className='about_card bg-tema-secondary border-[1px] border-solid border-transparent rounded-2xl p-8 text-center hover:bg-transparent hover:border-tema-secondary hover:cursor-default'>
-              <FiUsers className='about_icon text-primary text-2xl mb-4 mx-auto' />
+            <article className='bg-tema-secondary border-[1px] border-solid border-transparent rounded-2xl p-8 text-center hover:bg-transparent hover:border-tema-secondary hover:cursor-default'>
+              <FiUsers className='text-primary text-2xl mb-4 mx-auto' />
               <h5 className="text-base">Clients</h5>
               <small className="text-xs text-light">200+ Worldwide</small>
             </article>
-            <article className='about_card bg-tema-secondary border-[1px] border-solid border-transparent rounded-2xl p-8 text-center hover:bg-transparent hover:border-tema-secondary hover:cursor-default'>
-              <VscFolderLibrary className='about_icon text-primary text-2xl mb-4 mx-auto' />
+            <article className='bg-tema-secondary border-[1px] border-solid border-transparent rounded-2xl p-8 text-center hover:bg-transparent hover:border-tema-secondary hover:cursor-default'>
+              <VscFolderLibrary className='text-primary text-2xl mb-4 mx-auto' />
               <h5 className="text-base">Projects</h5>
               <small className="text-xs text-light">80+ Completed</small>
             </article>
